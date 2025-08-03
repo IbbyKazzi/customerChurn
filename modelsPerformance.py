8bde14e27284ff88ff1c49b441da3c0c280ff622
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-04 08:20:20 +1000

    Update modelsPerformance.py

--- modelsPerformance.py
+++ modelsPerformance.py
@@ -5,34 +5,35 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-with open("model_top3.pkl", "rb") as f:
-    model_t3 = pickle.load(f)
-
-with open("model_all.pkl", "rb") as f:
-    model_all = pickle.load(f)
-
-#get the dataset
-df = pd.read_csv("encoded-dataset.csv")
-X = df_encoded.drop(['Churn', 'customerID'], axis=1)
-y = df_encoded['Churn']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Get predicted probabilities
-y_probs = model_t3.predict_proba(X_test)[:, 1]
-
-# Compute ROC curve
-fpr, tpr, thresholds = roc_curve(y_test, y_probs)
-
-# Compute AUC score
-auc_score = roc_auc_score(y_test, y_probs)
-fig, ax = plt.subplots()
-ax.plot(fpr, tpr, label=f"AUC = {auc_score:.2f}")
-ax.plot([0, 1], [0, 1], linestyle="--", color="gray")
-ax.set_xlabel("False Positive Rate")
-ax.set_ylabel("True Positive Rate")
-ax.set_title("ROC Curve")
-ax.legend()
-
-st.subheader("📊 ROC Curve Performance", key="per_sh1")
-st.pyplot(fig, key="per_pt1")
-st.metric(label="ROC AUC Score", value=f"{auc_score:.2f}", key="per_mt1")
+def run():    
+    with open("model_top3.pkl", "rb") as f:
+        model_t3 = pickle.load(f)
+    
+    with open("model_all.pkl", "rb") as f:
+        model_all = pickle.load(f)
+    
+    #get the dataset
+    df = pd.read_csv("encoded-dataset.csv")
+    X = df_encoded.drop(['Churn', 'customerID'], axis=1)
+    y = df_encoded['Churn']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Get predicted probabilities
+    y_probs = model_t3.predict_proba(X_test)[:, 1]
+    
+    # Compute ROC curve
+    fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+    
+    # Compute AUC score
+    auc_score = roc_auc_score(y_test, y_probs)
+    fig, ax = plt.subplots()
+    ax.plot(fpr, tpr, label=f"AUC = {auc_score:.2f}")
+    ax.plot([0, 1], [0, 1], linestyle="--", color="gray")
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.set_title("ROC Curve")
+    ax.legend()
+    
+    st.subheader("📊 ROC Curve Performance", key="per_sh1")
+    st.pyplot(fig, key="per_pt1")
+    st.metric(label="ROC AUC Score", value=f"{auc_score:.2f}", key="per_mt1")
