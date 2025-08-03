cd1871d64248e21f1b2ba80b232ac87486478a17
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-04 08:31:21 +1000

    Update modelsPerformance.py

--- modelsPerformance.py
+++ modelsPerformance.py
@@ -27,9 +27,11 @@ def run():
     
     # Drop rows with NaN values created by the conversion
     df_encoded.dropna(inplace=True)
-    X = df_encoded['tenure', 'MonthlyCharges', 'Contract']
+    top_features = ['tenure', 'MonthlyCharges', 'Contract']
+    X_top3 = df_encoded[top_features]
+    X_top3
     y = df_encoded['Churn']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_top3, y, test_size=0.2, random_state=42)
     
     # Get predicted probabilities
     y_probs = model_t3.predict_proba(X_test)[:, 1]
