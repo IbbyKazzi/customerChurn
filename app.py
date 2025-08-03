0287c288fa09c43722b7cb5c532de6cb79d484dd
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-03 13:53:20 +1000

    Update app.py

--- app.py
+++ app.py
@@ -33,18 +33,18 @@ def runHome():
     #if st.button("Go to Churn Analysis 📊"):
     #    st.switch_page("churn_analysis.py")  
     
-    github_url = "https://customerchurn-utsp1-analysis.streamlit.app/"
+    #github_url = "https://customerchurn-utsp1-analysis.streamlit.app/"
     
-    st.markdown(
-        f"""
-        <a href="{github_url}" target="_blank">
-            <button style="background-color:#1f77b4; color:white; padding:10px 24px; font-size:16px; border:none; border-radius:5px;">
-                🚀 Go to Churn Analysis 📊
-            </button>
-        </a>
-        """,
-        unsafe_allow_html=True
-    )
+    #st.markdown(
+    #    f"""
+    #    <a href="{github_url}" target="_blank">
+    #        <button style="background-color:#1f77b4; color:white; padding:10px 24px; font-size:16px; border:none; border-radius:5px;">
+    #            🚀 Go to Churn Analysis 📊
+    #        </button>
+    #    </a>
+    #    """,
+    #    unsafe_allow_html=True
+    #)
 
 selected = option_menu(
     menu_title=None,
@@ -63,6 +63,7 @@ elif selected == "Cust Service":
 elif selected == "Retention":
     st.title("🛡️ Retention Page")
     run_retention()
+
 
 
 
