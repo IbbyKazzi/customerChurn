bb83ba54a081fba98e6f08bf0d3968723ea34585
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-04 15:16:01 +1000

    Update customerService.py

--- customerService.py
+++ customerService.py
@@ -90,6 +90,7 @@ def run():
     new_prob = get_newProb(override, tenure, customer_contract, model_t3)
     #st.write(new_prob)
     st.markdown(f"**Estimated Churn Probability for {override} Plan:** {new_prob:.2%}")
+    styled_metric("Estimated Churn Probability for {override} Plan:", churn_percent )
     
     #customer info display
     with st.expander("Customer History", expanded=False):
