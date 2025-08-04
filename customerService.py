7f6514010e3047f7115c47c82ec7bd2304ae0b37
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-04 12:53:25 +1000

    Update customerService.py

--- customerService.py
+++ customerService.py
@@ -6,77 +6,78 @@ import shap
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
-#get the prediction model
-with open("model_all.pkl", "rb") as f:
-    model = pickle.load(f)
-#import the dataset
-X = pd.read_csv("encoded-dataset.csv")
-explainer = shap.Explainer(model, X)
-shap_values = explainer(X)
-
-# Load your dataset to extract customer ids
-df = pd.read_csv("Customer-Churn-dataset.csv")
-df_filtered = df[df['Churn'] == 'No']
-
-# Extract unique customer IDs
-customer_ids_df = df_filtered['customerID'].reset_index()
-
-
-#set the page menu  Customer-Churn-dataset.csv
-st.sidebar.header("Customer Filter")
-# Choose the customer index
-selected_customer_id = st.sidebar.selectbox("Enter Customer ID", options=customer_ids_df['customerID'])
-
-# Now get the original index from the df
-i = customer_ids_df[customer_ids_df['customerID'] == selected_customer_id]['index'].values[0]
-
-
-#get selected customer's tenure,monthly charge and contract and use our prediction model to check churn possibility
-tenure = df.iloc[i]["tenure"]
-monthly_charges = df.iloc[i]["MonthlyCharges"]
-contract = df.iloc[i]["Contract"]
-#get the top 3 prediction model
-with open("model_top3.pkl", "rb") as f:
-    model_t3 = pickle.load(f)
-# encode categorical input of contract
-contract_map = {"Month-to-month": 0, "One year": 1, "Two year": 2}
-input_data = np.array([[tenure, monthly_charges, contract_map[contract]]])
-prediction = model_t3.predict_proba(input_data)
-churn_probability = prediction[0][1]
-churn_percent = f"{churn_probability:.0%}"
-#st.success(f"Predicted Churn: {'Yes' if prediction[0] == 1 else 'No'}")
-
-#add summary to the top of the page
-st.title("Churn Prevention & Plan Recommendation App")
-st.subheader(f"Customer ID: {selected_customer_id}")
-st.metric(label="Churn Risk", value=churn_percent, delta="-3% from last month")
-
-#factors of churn
-# Create a waterfall plot for that customer
-fig, ax = plt.subplots()
-shap.plots.waterfall(shap_values[i], show=False)
-st.pyplot(fig)
-#Below we can display the customers feature in a table form
-#st.write("Customer Features:")
-#st.dataframe(X.iloc[i:i+1])
-
-
-#Recommend Plan
-
-st.markdown("### Recommended Retention Plan")
-st.info("Switch to 'SecureNet Plus': 24-month contract, free upgrade, loyalty rewards.")
-
-# Optional dropdown to allow manual override
-available_plans = ["Basic", "Premium", "Family", "Enterprise"]
-override = st.selectbox("Override Plan Suggestion", options=available_plans)
-  
-#customer info display
-with st.expander("Customer History", expanded=False):
-    st.write(df.iloc[i])
-
-#check recommandation outcome
-st.markdown("#### Recommendation Outcome")
-st.radio("Was the recommendation accepted?", ["Yes", "No", "Pending"])
-st.text_area("Agent Notes")
-st.button("Submit Feedback")
-  
+def run():
+    #get the prediction model
+    with open("model_all.pkl", "rb") as f:
+        model = pickle.load(f)
+    #import the dataset
+    X = pd.read_csv("encoded-dataset.csv")
+    explainer = shap.Explainer(model, X)
+    shap_values = explainer(X)
+    
+    # Load your dataset to extract customer ids
+    df = pd.read_csv("Customer-Churn-dataset.csv")
+    df_filtered = df[df['Churn'] == 'No']
+    
+    # Extract unique customer IDs
+    customer_ids_df = df_filtered['customerID'].reset_index()
+    
+    
+    #set the page menu  Customer-Churn-dataset.csv
+    st.sidebar.header("Customer Filter")
+    # Choose the customer index
+    selected_customer_id = st.sidebar.selectbox("Enter Customer ID", options=customer_ids_df['customerID'])
+    
+    # Now get the original index from the df
+    i = customer_ids_df[customer_ids_df['customerID'] == selected_customer_id]['index'].values[0]
+    
+    
+    #get selected customer's tenure,monthly charge and contract and use our prediction model to check churn possibility
+    tenure = df.iloc[i]["tenure"]
+    monthly_charges = df.iloc[i]["MonthlyCharges"]
+    contract = df.iloc[i]["Contract"]
+    #get the top 3 prediction model
+    with open("model_top3.pkl", "rb") as f:
+        model_t3 = pickle.load(f)
+    # encode categorical input of contract
+    contract_map = {"Month-to-month": 0, "One year": 1, "Two year": 2}
+    input_data = np.array([[tenure, monthly_charges, contract_map[contract]]])
+    prediction = model_t3.predict_proba(input_data)
+    churn_probability = prediction[0][1]
+    churn_percent = f"{churn_probability:.0%}"
+    #st.success(f"Predicted Churn: {'Yes' if prediction[0] == 1 else 'No'}")
+    
+    #add summary to the top of the page
+    st.title("Churn Prevention & Plan Recommendation App")
+    st.subheader(f"Customer ID: {selected_customer_id}")
+    st.metric(label="Churn Risk", value=churn_percent, delta="-3% from last month")
+    
+    #factors of churn
+    # Create a waterfall plot for that customer
+    fig, ax = plt.subplots()
+    shap.plots.waterfall(shap_values[i], show=False)
+    st.pyplot(fig)
+    #Below we can display the customers feature in a table form
+    #st.write("Customer Features:")
+    #st.dataframe(X.iloc[i:i+1])
+    
+    
+    #Recommend Plan
+    
+    st.markdown("### Recommended Retention Plan")
+    st.info("Switch to 'SecureNet Plus': 24-month contract, free upgrade, loyalty rewards.")
+    
+    # Optional dropdown to allow manual override
+    available_plans = ["Basic", "Premium", "Family", "Enterprise"]
+    override = st.selectbox("Override Plan Suggestion", options=available_plans)
+      
+    #customer info display
+    with st.expander("Customer History", expanded=False):
+        st.write(df.iloc[i])
+    
+    #check recommandation outcome
+    st.markdown("#### Recommendation Outcome")
+    st.radio("Was the recommendation accepted?", ["Yes", "No", "Pending"])
+    st.text_area("Agent Notes")
+    st.button("Submit Feedback")
+      
