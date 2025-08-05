722de86907247279dc96a2c182da3a7cc65ed012
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-05 18:50:26 +1000

    Update modelsPerformance.py

--- modelsPerformance.py
+++ modelsPerformance.py
@@ -7,8 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 def run(): 
-    path1 = r"/mount/src/customerchurn/models/model_3_v2.pkl"
-    path2 = r"/mount/src/customerchurn/models/model_20_v2.pkl"
+    path1 = r"/mount/src/customerchurn/models/model_3_v3.pkl"
+    path2 = r"/mount/src/customerchurn/models/model_20_v3.pkl"
     #st.write(path2)
     with open(path1, "rb") as f:
         model_t3 = pickle.load(f)
@@ -34,7 +34,7 @@ def run():
     
     # Fill NaNs with median
     median_value = df['TotalCharges'].median()
-    df['TotalCharges'].fillna(median_value)
+    df['TotalCharges'] = df['TotalCharges'].fillna(median_value)
     
     # Encode categorical variables
     df_encoded = df.copy()
