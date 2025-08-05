8bc476d253ef17a28875d672cfbe293d2d3d2dcc
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-05 15:40:34 +1000

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,8 @@ selected = option_menu(
 
 if selected == "Home":
     st.title("🏠 Home Page")
-    #import home
-    #home.run()
+    import home_page
+    home_page.run()
 elif selected == "Analysis":
     st.title("📊 Analysis Page")
     import churn_analysis
@@ -154,6 +154,7 @@ elif selected == "Cust Service":
 elif selected == "Retention":
     st.title("🛡️ Retention Page")
     run_retention()
+
 
 
 
