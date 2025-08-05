6eb87f6746e43aac97f7670be543eb7c14bdb141
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-05 15:31:32 +1000

    Update app.py

--- app.py
+++ app.py
@@ -11,26 +11,26 @@ import plotly.express as px
 
 
 def runHome2():    
-    with open("model_top3.pkl", "rb") as file:
-        model = pickle.load(file)
-    st.subheader("Churn Prediction Model")
-    st.subheader("UTS P1 - 2025")
-    st.write("Authors: Murray Atkin, Ibrahim Kassem, Bradley Moore, Preeti Sowrab")
+    #with open("model_top3.pkl", "rb") as file:
+    #    model = pickle.load(file)
+    #st.subheader("Churn Prediction Model")
+    #st.subheader("UTS P1 - 2025")
+    #st.write("Authors: Murray Atkin, Ibrahim Kassem, Bradley Moore, Preeti Sowrab")
 
-    #st.subheader("💡 Prediction calculator")
-    st.markdown("<h4 style='font-size:18px;'>💡 Prediction calculator</h3>", unsafe_allow_html=True)
-    tenure = st.number_input("Tenure", min_value=0, step=1, format="%d") #get tenure as a whole number
-    monthly_charges = st.number_input("Monthly Charges") #get monthly charges as a decimal number
-    contract_type = st.selectbox("Contract Type", ["Month-to-month", "One year", "Two year"])
+    ##st.subheader("💡 Prediction calculator")
+    #st.markdown("<h4 style='font-size:18px;'>💡 Prediction calculator</h3>", unsafe_allow_html=True)
+    #tenure = st.number_input("Tenure", min_value=0, step=1, format="%d") #get tenure as a whole number
+    #monthly_charges = st.number_input("Monthly Charges") #get monthly charges as a decimal number
+    #contract_type = st.selectbox("Contract Type", ["Month-to-month", "One year", "Two year"])
     
-    # encode categorical input of contract
-    contract_map = {"Month-to-month": 0, "One year": 1, "Two year": 2}
-    input_data = np.array([[tenure, monthly_charges, contract_map[contract_type]]])
-    if st.button("Predict"):
-        prediction = model.predict(input_data)
-        st.success(f"Predicted Churn: {'Yes' if prediction[0] == 1 else 'No'}")
-    import modelsPerformance
-    modelsPerformance.run()    
+    ## encode categorical input of contract
+    #contract_map = {"Month-to-month": 0, "One year": 1, "Two year": 2}
+    #input_data = np.array([[tenure, monthly_charges, contract_map[contract_type]]])
+    #if st.button("Predict"):
+    #    prediction = model.predict(input_data)
+    #    st.success(f"Predicted Churn: {'Yes' if prediction[0] == 1 else 'No'}")
+    #import modelsPerformance
+    #modelsPerformance.run()    
 
 def run_retention():
     #get the prediction model
@@ -154,6 +154,7 @@ elif selected == "Cust Service":
 elif selected == "Retention":
     st.title("🛡️ Retention Page")
     run_retention()
+
 
 
 
