f8b62ccdf789de1e2a3b871e7aa2e3e60382acbd
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-06 16:54:29 +1000

    Update modelsPerformance.py

--- modelsPerformance.py
+++ modelsPerformance.py
@@ -16,50 +16,11 @@ def run():
     with open(path2, "rb") as f:
         model_all = pickle.load(f)
     
-    #get the dataset
-    df = pd.read_csv("Customer-Churn-dataset.csv")  
-
-    # Convert column to numeric (in case it's still object type) and fill in missing values
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
     
-    # Fill NaNs with median
-    median_value = df['TotalCharges'].median()
-    df['TotalCharges'] = df['TotalCharges'].fillna(median_value)
-
-    #Features Engineering
-    # tenure group in 3 categories, New - Loyal - Long-term
-    def tenure_group(tenure):
-        if tenure <= 12:
-            return 'New'
-        elif 12 < tenure <= 24:
-            return 'Loyal'
-        else:
-            return 'Long-term'
-    
-    df['tenure_group'] = df['tenure'].apply(tenure_group)
-
-    #add another feature
-    df['charge_ratio'] = df['MonthlyCharges'] / (df['TotalCharges'] + 1e-5)
-    
-    # Encode categorical variables
-    df_encoded = df.copy()
-    for col in df_encoded.select_dtypes(include='object').columns:
-        # Exclude 'TotalCharges' for now as it seems to have non-numeric values that need handling
-        if col not in ['customerID', 'TotalCharges']:
-            df_encoded[col] = LabelEncoder().fit_transform(df_encoded[col])
-    
-    # Convert 'TotalCharges' to numeric, coercing errors to NaN
-    df_encoded['TotalCharges'] = pd.to_numeric(df_encoded['TotalCharges'], errors='coerce')
-    
-    # Drop rows with NaN values created by the conversion
-    df_encoded.dropna(inplace=True)
-    df_encoded.columns = df_encoded.columns.str.strip()
-    df_encoded.drop('customerID', axis=1, inplace=True)
-
+    #load the dataset
     import load_dataset
-    X_All = load_dataset.run()
-    X_All = X_All.drop(['Churn'], axis=1)
-    #X_All = df_encoded.drop(['Churn'], axis=1)
+    df_encoded = load_dataset.run()    
+    X_All = df_encoded.drop(['Churn'], axis=1)   
     
     top_features = ['tenure', 'MonthlyCharges', 'Contract']
     X_top3 = df_encoded[top_features]    
