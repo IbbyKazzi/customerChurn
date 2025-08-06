bc98125121cee47e44c08cf75965ffdba97d49c2
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-06 17:54:50 +1000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 st.markdown("""
     <style>
     .option-menu-container {
-        max-width: 100%;
+        min-width: 100%;
         margin: auto;
     }
     </style>
@@ -37,6 +37,7 @@ elif selected == "Retention":
     st.sidebar.header("🛡️ Retention")
     import retention
     retention.run()
+
 
 
 
