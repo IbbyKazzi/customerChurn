62c67f0eb1a45eec8077106d807439766386a64d
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-07 08:39:13 +1000

    Update retention.py

--- retention.py
+++ retention.py
@@ -124,7 +124,7 @@ def saveToGithub(df):
         
     # GitHub credentials
     token = "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"
-    repo_name = "IbbyKazzi/customerchurn"
+    repo_name = "IbbyKazzi/customerChurn "
     file_path = "data/customer_churn_data.csv"
     commit_message = "Update churn data to Github"
         
