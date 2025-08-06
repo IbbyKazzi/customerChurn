fdc47f02d1c6b9441bb9d0dd556cc3cedddc4473
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-06 16:45:54 +1000

    Update load-dataset.py

--- load-dataset.py
+++ load-dataset.py
@@ -1,3 +1,12 @@
-import pandas as py
+import pickle
+from sklearn.metrics import roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
+
+def run():
+  
 
