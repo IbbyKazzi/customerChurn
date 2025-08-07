0b4f52ba45a8a868b71dd8563477e3d2980f3f7f
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-07 15:48:43 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,5 @@ MODEL_PATH_T3 = r"/mount/src/customerchurn/models/model_3_v4.pkl"
 MODEL_PATH_T21 = r"/mount/src/customerchurn/models/model_21_v4.pkl"
 DATA_PATH = r"/mount/src/customerchurn/data/customer_churn_data.csv"
 DEBUG_MODE = True
-token = "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"
 repo_name = "IbbyKazzi/customerChurn "
 file_path = "data/customer_churn_data.csv"
