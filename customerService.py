d9c340c0e60fe7cfdfe06621ca37bc8fa435a7d3
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-09 14:35:41 +1000

    Update customerService.py

--- customerService.py
+++ customerService.py
@@ -66,16 +66,9 @@ def run():
         "monthlyCharges": monthly_charges
     }
     import customerServiceAssistance
-    customerServiceAssistance.run(customer, shap_values, X)
+    customerServiceAssistance.run(customer, shap_values, X, i)
     
-    #factors of churn
-    # Create a waterfall plot for that customer
-    st.markdown("<div style='margin-top: 20px'></div>", unsafe_allow_html=True)
-    if st.toggle("Show churn factor waterfall"):
-        st.markdown("### Factors of Churn")
-        fig, ax = plt.subplots()
-        #shap.plots.waterfall(shap_values[i], show=False)
-        #st.pyplot(fig)
+    
 
     #fig, ax = plt.subplots()
     #shap.plots.waterfall(shap_values[i], show=False)
