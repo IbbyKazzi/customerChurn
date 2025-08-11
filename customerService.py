9210c596a3db690c42b77ebcd12be746f35a14bd
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-11 20:24:54 +1000

    Update customerService.py

--- customerService.py
+++ customerService.py
@@ -88,7 +88,11 @@ def run():
     if selected_customer_id != st.session_state.prev_customer_id:
         st.session_state.prev_customer_id = selected_customer_id
         # 🔁 Call your function here
-        def on_customer_change(customer_id):            
+        def on_customer_change(customer_id): 
+            if "show_message" not in st.session_state:
+                st.session_state["show_message"] = True
+            else:
+                st.session_state["show_message"] = True
             import customerServiceAssistance
             customerServiceAssistance.run(customer, shap_values[i], X, contract_map, df)
         on_customer_change(selected_customer_id) 
