e4210002e35fc14016f6d636c35518bd5a3201ed
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-11 20:04:02 +1000

    Update customerServiceAssistance.py

--- customerServiceAssistance.py
+++ customerServiceAssistance.py
@@ -39,7 +39,7 @@ def run(customer, shap_values, X, contract_map, df):
     # Create a DataFrame for easy sorting
     feature_importance = pd.DataFrame({
         'feature': X.columns,
-        'importance': np.abs(shap_values)
+        'importance': np.abs(shap_values.values)
     }).sort_values(by='importance', ascending=False)
 
     
