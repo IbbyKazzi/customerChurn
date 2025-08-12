7c01c2b4f307c6e52ac6401e0b59d6f36cd983a9
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-12 11:05:03 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # settings.py
 
-MODEL_PATH_T3 = r"/mount/src/customerchurn/models/model_3_v5.pkl"
-MODEL_PATH_T21 = r"/mount/src/customerchurn/models/model_21_v5.pkl"
+MODEL_PATH_T3 = r"/mount/src/customerchurn/models/model_3_V5.pkl"
+MODEL_PATH_T21 = r"/mount/src/customerchurn/models/model_21_V5.pkl"
 DATA_PATH = r"/mount/src/customerchurn/data/customer_churn_data.csv"
 METADATA_PATH = r"/mount/src/customerchurn/models/model_metadata.json"
 DEBUG_MODE = True
