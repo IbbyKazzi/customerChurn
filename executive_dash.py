b0bf882f6da25887b4038bdf7f97c8a4d3245455
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-20 12:02:09 +1000

    Update executive_dash.py

--- executive_dash.py
+++ executive_dash.py
@@ -82,9 +82,9 @@ def run():
    
     df['Contract_Month-to-month'] = (df['Contract'] == 0).astype(int)
     df['Contract_One_Year'] = (df['Contract'] == 1).astype(int)
-    df['InternetService_Fiber optic'] = (df['InternetService'] == 2).astype(int)
+    df['InternetService_Fiber optic'] = (df['InternetService'] == 1).astype(int)
     df['TechSupport_No'] = (df['TechSupport'] == 0).astype(int)
-    df['PaymentMethod_Electronic check'] = (df['PaymentMethod'] == 0).astype(int)
+    df['PaymentMethod_Electronic check'] = (df['PaymentMethod'] == 2).astype(int)
 
     st.write(df)
 
