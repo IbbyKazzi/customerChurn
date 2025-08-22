e72f145c90a60c1794fca69cfd1cc7dd229ac295
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-22 14:40:36 +1000

    Update automated_pipeline.py

--- automated_pipeline.py
+++ automated_pipeline.py
@@ -68,7 +68,7 @@ def train_models(X_train, y_train):
     #    "LogisticRegression": LogisticRegression(C=0.1, penalty='l1', solver='liblinear', max_iter=1000),
     #    "RandomForest": RandomForestClassifier(n_estimators=100)
     #}
-    with open(MODEL_PATH_T31, "rb") as f:
+    with open(MODEL_PATH_T21, "rb") as f:
         model_t21 = pickle.load(f)
     models = {
         "Trained_Model": LogisticRegression(C=0.1, penalty='l1', solver='liblinear', max_iter=1000),
