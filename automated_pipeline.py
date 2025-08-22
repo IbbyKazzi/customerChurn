2a596543e12c0e950ae754f277abbb96127b2f7c
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-22 14:53:42 +1000

    Change LogisticRegression penalty to 'l2'

--- automated_pipeline.py
+++ automated_pipeline.py
@@ -66,7 +66,7 @@ def tenure_group(tenure):
 #Model Training (logistic & Randomforest)
 def train_models(X_train, y_train):
     #models = {
-    #    "LogisticRegression": LogisticRegression(C=0.1, penalty='l1', solver='liblinear', max_iter=1000),
+    #    "LogisticRegression": LogisticRegression(C=0.1, penalty='l2', solver='liblinear', max_iter=1000),
     #    "RandomForest": RandomForestClassifier(n_estimators=100)
     #}
     with open(MODEL_PATH_T21, "rb") as f:
