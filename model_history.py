b9f34f58c49dc8d4b60d4f84abb1d91cab291451
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-29 18:45:06 +1000

    Update model_history.py

--- model_history.py
+++ model_history.py
@@ -68,8 +68,8 @@ def show_model_history(path=METADATA_PATH):
             selected_features, ffs_scores = ap.forward_feature_selection(
                 pd.DataFrame(X_train_full, columns=X_df.columns), y_train
             )
-            st.write("Selected features:", selected)
-            st.line_chart(scores)
+            st.write("Selected features:", selected_features)
+            st.line_chart(ffs_scores)
 
     
             from feature_store.registry import save_selected_features
