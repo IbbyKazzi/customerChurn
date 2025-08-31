15cd207cf07a3790045f23f3b67adc9c3514d7d7
Ibrahim Kassem <ihkassem82@gmail.com>
2025-08-31 20:34:27 +1000

    Refactor save results button display logic

--- model_history.py
+++ model_history.py
@@ -114,11 +114,13 @@ def run():
         st.dataframe(st.session_state.scores_df)
         st.plotly_chart(st.session_state.fig, use_container_width=True)
         st.success("✅ Pipeline completed!")
+        #display save button after pipeline complition
+        if st.button("💾 Save Results"):
+            st.session_state.save_results = True
 
         
 
-    if st.button("💾 Save Results"):
-        st.session_state.save_results = True
+    
 
     if st.session_state.save_results:
         st.write("start saving to guithub")
