06e55a6d41023c99b53f2939ff3a743240f37470
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-01 18:28:31 +1000

    Refactor feature selection logic in automated_pipeline.py

--- automated_pipeline.py
+++ automated_pipeline.py
@@ -47,13 +47,13 @@ def forward_feature_selection(X, y, max_features=5):
         score_candidates.sort(reverse=True)
         best_new_score, best_feature = score_candidates[0]
 
-        if best_new_score > best_score:
-            selected.append(best_feature)
-            remaining.remove(best_feature)
-            best_score = best_new_score
-            scores.append(best_score)
-        else:
-            break
+        #if best_new_score > best_score:
+        selected.append(best_feature)
+        remaining.remove(best_feature)
+            #best_score = best_new_score
+        scores.append(best_score)
+        #else:
+        #    break
     
     return selected, scores
 
