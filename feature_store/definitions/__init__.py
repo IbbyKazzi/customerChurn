a1f96c170cf1a29e20d93c4fc0dc22b842c99472
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-01 18:05:18 +1000

    Add contract_stability to feature store definitions

--- feature_store/definitions/__init__.py
+++ feature_store/definitions/__init__.py
@@ -2,5 +2,6 @@
 
 from .loyalty_band import loyalty_band
 from .charge_velocity import charge_velocity
+from .contract_stability import contract_stability
 
 __all__ = ['loyalty_band', 'charge_velocity', 'contract_stability']
