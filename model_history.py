85e029601888b26c35e2f00443383f0b4aa8b17a
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-02 17:32:20 +1000

    Update model_history.py

--- model_history.py
+++ model_history.py
@@ -133,7 +133,7 @@ def run():
 
         with st.expander("🔍 View Grid search HPO"):
             st.caption("🔧 HPO used")
-            st.json(st.session_state.grid_search)
+            st.write(st.session_state.grid_search)
 
         end_time = time.time()
         elapsed = end_time - start_time
