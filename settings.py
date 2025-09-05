7651165616e60bdd6219f4046d7a5ceda6cf8693
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-05 12:00:03 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 # settings.py
 
-MODEL_PATH_T3 = r"/mount/src/customerchurn/models/model_3_V5.pkl"
-MODEL_PATH_T21 = r"/mount/src/customerchurn/models/model_21_V5.pkl"
+MODEL_PATH_T3 = r"models/model_3_V5.pkl"
+MODEL_PATH_T21 = r"models/model_21_V5.pkl"
 DATA_PATH = r"data/customer_churn_data.csv"
-METADATA_PATH = r"/mount/src/customerchurn/models/model_metadata.json"
-MODEL_SAVE_DIR = r"/mount/src/customerchurn/models"
+METADATA_PATH = r"models/model_metadata.json"
+MODEL_SAVE_DIR = r"models"
 DEBUG_MODE = True
 repo_name = "IbbyKazzi/customerChurn "
 file_path = "data/customer_churn_data.csv"
