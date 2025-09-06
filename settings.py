cf57f11e709517b097c60acadae525fb232a33ea
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-06 16:48:50 +1000

    Add model performance path to settings

--- settings.py
+++ settings.py
@@ -5,6 +5,8 @@ MODEL_PATH_T21 = r"models/model_21_V5.pkl"
 DATA_PATH = r"data/customer_churn_data.csv"
 METADATA_PATH = r"models/model_metadata.json"
 MODEL_SAVE_DIR = r"models"
+MODEL_PERFORMANCE_PATH = r"models/model_performance.json"
 DEBUG_MODE = True
+
 repo_name = "IbbyKazzi/customerChurn "
 file_path = "data/customer_churn_data.csv"
