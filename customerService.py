d7772d7b6cbf65493f65400ab34a8bbe9f14a151
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-10 20:08:10 +1000

    Fix explainer to use model instead of pipeline

--- customerService.py
+++ customerService.py
@@ -30,7 +30,7 @@ def run():
     y = df_encoded['Churn']
     X_train, X_test, y_train, y_test = train_test_split(X_All, y, test_size=0.2, random_state=42)
     
-    explainer = shap.Explainer(pipeline.predict_proba, X_train)
+    explainer = shap.Explainer(model.predict_proba, X_train)
     shap_values = explainer(X_test)
 
 
