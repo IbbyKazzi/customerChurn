0db0f057d5899ea4c5d9ba59a089d597230301ed
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-11 14:45:18 +1000

    Update customerServiceAssistance.py

--- customerServiceAssistance.py
+++ customerServiceAssistance.py
@@ -118,6 +118,7 @@ def run(customer, shap_values, X, contract_map, df, newCustomer):
             st.session_state["new_question"] = question
 
         if question and st.session_state["new_question"] != question:
+            st.session_state["new_question"] = question
             st.session_state["show_question"] = True
             generate_response(question, customer, shap_values, contract_map, df)
         
