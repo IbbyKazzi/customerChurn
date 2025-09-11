42bf46f962927a36545d35418c5e5a391504898b
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-11 14:46:28 +1000

    Display current question and new question state
    Add debug output for the current question state.

--- customerServiceAssistance.py
+++ customerServiceAssistance.py
@@ -117,6 +117,8 @@ def run(customer, shap_values, X, contract_map, df, newCustomer):
         else:
             st.session_state["new_question"] = question
 
+        st.write(question)
+        st.write(st.session_state["new_question"])
         if question and st.session_state["new_question"] != question:
             st.session_state["new_question"] = question
             st.session_state["show_question"] = True
