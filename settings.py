5e9aaf7c6063715ede8992f2cde58656b230845e
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-15 14:57:24 +1000

    🔧 Update MODEL_PATH_T21 to latest model

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # settings.py
 
 MODEL_PATH_T3 = r"models/logreg_model_3f.pkl"
-MODEL_PATH_T21 = r"models/logreg_model_20250911_183256.pkl"
+MODEL_PATH_T21 = r"models/logreg_model_20250915_145701.pkl"
 DATA_PATH = r"data/customer_churn_data.csv"
 METADATA_PATH = r"models/model_metadata.json"
 MODEL_SAVE_DIR = r"models"
