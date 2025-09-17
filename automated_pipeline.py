94a7c54152163125c4d34421e30172bbfbadb4df
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-17 17:06:20 +1000

    Update automated_pipeline.py

--- automated_pipeline.py
+++ automated_pipeline.py
@@ -212,7 +212,7 @@ def train_models(X_train, y_train, X_test, y_test, current_model_name):
     model_name = f"logreg_model_{date_str}"
     models = {        
         model_name : best_model # the best model with grid search HPO
-        #current_model_name : model_t21 # Our currently used model
+        current_model_name : model_t21 # Our currently used model
     }
 
     for name, model in models.items():
