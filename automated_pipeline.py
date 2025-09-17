11122e692379d8b968072005493b596543d043b3
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-17 17:07:20 +1000

    Fix formatting in models dictionary assignment

--- automated_pipeline.py
+++ automated_pipeline.py
@@ -211,7 +211,7 @@ def train_models(X_train, y_train, X_test, y_test, current_model_name):
     date_str = now_sydney.strftime("%Y%m%d_%H%M%S")
     model_name = f"logreg_model_{date_str}"
     models = {        
-        model_name : best_model # the best model with grid search HPO
+        model_name : best_model, # the best model with grid search HPO
         current_model_name : model_t21 # Our currently used model
     }
 
