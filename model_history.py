6c25e36b460103e3c4090d4e14c5754e20e53893
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-17 17:14:46 +1000

    Update model_history.py

--- model_history.py
+++ model_history.py
@@ -231,7 +231,7 @@ def run():
         end_time = time.time()
         elapsed = end_time - st.session_state.start_time  
         if st.session_state.best_model_auc > st.session_state.auc_threshold:
-            annotation = "🆕 (new model)" if st.session_state.best_model == st.session_state.new_model else ""
+            annotation = "🆕 (new model)" if st.session_state.best_model_index == 0 else ""
             st.success(
                 f"✅ Pipeline completed, with best model: {st.session_state.best_model} {annotation} and AUC: {st.session_state.best_model_auc:.4f}"
             )
