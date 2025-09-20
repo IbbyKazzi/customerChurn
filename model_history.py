f1dbb3fc0edaf16318701db2c324c987a5592782
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-20 17:22:31 +1000

    Update model_history.py

--- model_history.py
+++ model_history.py
@@ -330,10 +330,10 @@ def run():
             st.toast("📘 Registry entry saved", icon="📚", duration=10)
             
             # Save selected features and model
-            st.sidebar.write(st.session_state.best_model_name)
-            st.sidebar.write(st.session_state.best_model)            
+            #st.sidebar.write(st.session_state.best_model_name)
+            #st.sidebar.write(st.session_state.best_model)            
             save_selected_features("logistic_ffs", st.session_state.selected_features)
-            saveToGit("logistic_ffs", model_obj, st.session_state.best_model_name)
+            saveToGit("logistic_ffs", model_obj, model_filename)
             st.sidebar.success("✅ Features saved to GitHub successfully!")
             st.toast("📁 logistic_ffs.json uploaded", icon="📤", duration=10)
 
