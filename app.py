192e3ec912251702345a57603d02a7758f27c678
Ibrahim Kassem <ihkassem82@gmail.com>
2025-09-24 19:51:36 +1000

    Refactor app.py for improved structure and styling

--- app.py
+++ app.py
@@ -159,18 +159,17 @@ def get_base64_image(path):
 
 logo_base64 = get_base64_image("assets/logo2.png")
 
-
+# add logo and app name and  slogan
 st.sidebar.markdown(
     f"""
     <div style='text-align: center;'>
         <img src='data:image/png;base64,{logo_base64}' width='120'><br>
         <h3 style='margin-top: 5px; font-size: 18px;'>Churn Reduction App</h3>
+        <p style='font-size:14px; color:gray;'>Predict. Prevent. Retain.</p>
     </div>
     """,
     unsafe_allow_html=True
 )
-
-<p style='font-size:14px; color:gray;'>Predict. Prevent. Retain.</p>
 
 
 
@@ -290,6 +289,7 @@ with col2:
         "</div>",
         unsafe_allow_html=True
     )
+
 
 
 
